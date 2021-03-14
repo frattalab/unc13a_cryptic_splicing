dad26f6709a2660eb4f0427d56b52252b9cdf79d
oscar <oscargwilkins@gmail.com>
2021-03-14 18:17:28 +0000

    add preprocessing script for random hexamer targeted rnaseq

--- preprocessing_scripts/Targeted_RNA_seq/random_hexamer/analyse_unc_trnaseq_3.R
+++ preprocessing_scripts/Targeted_RNA_seq/random_hexamer/analyse_unc_trnaseq_3.R
@@ -5,15 +5,14 @@ library(clustringr)
 library(stringdist)
 
 # The purpose of this script is to find the number of unique UMIs  (i.e. unique cDNAs) in the 
-# targeted RNA sequencing of UNC13A, using a specific RT primer.
+# targeted RNA sequencing of UNC13A, using a random hexamer RT primer.
 #
 # This script takes .bam files aligned to the human (HG38) genome. It filters for alignments near
 # the UNC13A cryptic exon. It works out how many unique UMIs there are, and whether these 
 # cDNAs contain the risk or non-risk alleles of rs12973192
 #
-# The script first identifies whether reads contain the cryptic exon, or are "correctly" spliced. 
-# It then extracts the UMIs associated with each read from the read name (these were already moved
-# from the read header during demultiplexing with Ultraplex)
+# The script extracts the UMIs associated with each read from the read name (these were already moved
+# from the read header during demultiplexing with Ultraplex), and checks each read aligns to the cryptic.
 #
 # Because these libraries were very low complexity/over-amplified (especially for the reads which contained the 
 # cryptic exon) there is a high risk of erroneous false-positive UMIs due to sequencing/PCR errors.
@@ -26,53 +25,69 @@ library(stringdist)
 # the most similar UMI. It also requires that each UMI is at least 0.01x times as abundant
 # as the most abundant, and is present in at least 50 reads.
 #
-# This script differs from the random hexamer script in that it detects whether reads are cryptic
-# or correctly spliced by looking at the CIGAR string.
+# This script differs from the specific RT script in that it does not detect correctly spliced transcripts.
+# It also doesn't have a lower limit on the number of reads containing each UMI (though 
+# each must still be at least 0.01x the abundance of the most abundant).
+#
+# Note that although the forward reads in the fastq files for these samples correspond to the forward
+# strand of the RNA, and thus the MINUS strand of the genome, the bam file format automatically reverse
+# complements the reads, thus "C" is still healthy, and "G" is still risk.
+
 
 get_sample <- function(string){
-    # Detects sample name from the file name
     return(str_split(string,":")[[1]][2])
 }
 
-find_unique_umis <- function(df, distance_multiplier = 20, break_ratio = 100,
-                             min_n = 50){
+find_unique_umis <- function(df, distance_multiplier = 20, break_ratio = 100){
     # df should have two columns - "strings" which are the UMIs, and "n" which
     # is the copy number of each
     #
     # higher values of distance multiplier increase ability to detect low abundance
     # UMIs, at the risk of false positives. 100 works well for 1% error rate,
     # 250 is good for 0.2%
+    #
+    # for longer UMIs, there's increased risk of false positives for some reason
+    # so use a lower value
+    #
+    # break ratio - stop searching once UMIs are very low abundance
 
     df_remaining <- df
     df_added <- data.frame(strings = c(), n = c())
+
     biggest <- max(df$n)
 
     while(T){
-        
         # Find the one with the largest number
-        largest <- df_remaining$strings[which.max(df_remaining$n)] # the umi sequence
+        largest <- df_remaining$strings[which.max(df_remaining$n)]
         this_biggest <- max(df_remaining$n)
 
 
-        if(this_biggest < biggest/break_ratio | this_biggest < min_n){
+        if(this_biggest < biggest/break_ratio){
             break
         }
 
         this_n <- df_remaining$n[which.max(df_remaining$n)]
 
+
         # find its distance from others
         if(nrow(df_added) > 0){
-            
+
+
             df_added <- df_added %>%
                 mutate(distances = stringdist::stringdist(strings, largest))
-            
             min_distance <- min(df_added$distances[which(df_added$distances > 0)])
 
             # find how many there are of these
-            already_added_sum <- sum(df_added$n[which(df_added$distances==min_distance)])
+            already_added_sum <- sum(df_added$n[which(df_added$distances==min_distance)]) # PERHAPS THIS SHOULD BE ALL OF THEM? AND MAYBE SHOULD BE MAX
 
-        } else {  # if this is the first one
-            min_distance <- 8 # set to large so definitely included
+            # this_df <- df %>%
+            #   mutate(distances == stringdist(strings, largest)) %>%
+            #   filter(!distances == 0) %>% # remove self
+            #   filter(distances == min(distances))
+            # already_added_sum <- sum(this_df$n) # in this case not "already added", instead all of them
+
+        } else {
+            min_distance <- 8 # set to large so defo included
             already_added_sum <- 0
         }
 
@@ -84,6 +99,7 @@ find_unique_umis <- function(df, distance_multiplier = 20, break_ratio = 100,
                                       filter(strings == largest))
         }
 
+        # always remove regardless
         df_remaining <- df_remaining[which(!df_remaining$strings == largest),]
 
         if(nrow(df_remaining)%%100==0){
@@ -94,6 +110,7 @@ find_unique_umis <- function(df, distance_multiplier = 20, break_ratio = 100,
             break
         }
     }
+
     if(nrow(df_added) > 0){
         return(df_added %>% select(-distances))
     } else {
@@ -101,60 +118,56 @@ find_unique_umis <- function(df, distance_multiplier = 20, break_ratio = 100,
     }
 }
 
+
+
 ##### RUN
 
-# a csv containing the names of the samples
-samples <- read_csv("//data.thecrick.org/lab-ulej/home/users/wilkino/Unc13a/Targeted_RNAseq/barcodes2.csv",
-                   col_names = "sample_with_barcode")
+setwd("C:/Users/ogw/Google Drive/UCL PhD/Year 2/unc13a/analyse_targeted_RNASEQ/tg3/")
 
-# the directory containing the bam files
-dir <- "C:/Users/ogw/Downloads/bams/ultraplex_demux_"
+sample_names = read_csv("sample_names.csv")  # which barcode was used for each sample
 
-samples$sample <- unlist(lapply(samples$sample_with_barcode, get_sample))
+samples = data.frame(n = 1:14) %>% mutate(sample_name = paste0("A:/home/users/wilkino/Unc13a/tg3/ultraplex_demux_tg3_s",
+                                                               n, ".fastq.gzAligned.sortedByCoord.out.bam"))
 
-for(sample in samples$sample){
+for(sample in samples$n){
+    
+    bam_name = paste0("A:/home/users/wilkino/Unc13a/tg3/ultraplex_demux_tg3_s",
+                      sample, ".fastq.gzAligned.sortedByCoord.out.bam")
 
     message(sample)
-
-    bam_name <- paste0(dir,
-                        sample, ".fastq.gzAligned.out.sam.bam")
 
     bam_df <- data.frame(scanBam(bam_name))
 
     df2 <- bam_df %>%
         mutate(umi = str_sub(qname, str_length(qname)-7, str_length(qname))) %>%
-        filter(rname == "chr19", pos == 17641520) %>%  # position that primer "CATTGTTCTGCACGTCGGTC" starts from in HG38 genome
-        mutate(read_type = ifelse(str_detect(cigar, "37M857N48M"), "cryptic", "not_cryptic"))  # cryptic leaves gap of 857 nucleotides
+        filter(rname == "chr19", pos == 17641536) %>% # corresponds to primer GGTCACGAAGTGGAACAGG
+        mutate(read_type = ifelse(str_detect(cigar, "21M857N46M"), "cryptic", "not_cryptic")) # should have 857 nt gap if cryptic
     
-    # filter just for cryptic reads
-    umi_df <- df2 %>%
-        filter(read_type == "cryptic") %>%
-        select(umi) %>%
-        group_by(umi) %>%
-        mutate(umi_n = n()) %>% 
-        ungroup() %>%
-        select(umi, umi_n) %>% 
-        unique() %>%
-        filter(!str_detect(umi, "N")) %>%
-        arrange(desc(umi_n)) %>%
-        select(strings = umi, n = umi_n)
-
-    # Find UMIs which appear genuine
-    selected_umis <- find_unique_umis(umi_df, distance_multiplier = 10)
+    if(nrow(df2) > 20){
     
-    if(nrow(selected_umis) > 0){
-
+        umi_df <- df2 %>%
+            filter(read_type == "cryptic") %>%
+            select(umi) %>%
+            group_by(umi) %>%
+            mutate(umi_n = n()) %>% ungroup() %>%
+            select(umi, umi_n) %>% unique() %>%
+            filter(!str_detect(umi, "N")) %>%
+            arrange(desc(umi_n)) %>%
+            select(strings = umi, n = umi_n)
+        
+        # find genuine UMIs
+        selected_umis <- find_unique_umis(umi_df, distance_multiplier = 10)
+    
         # Now filter the bam_df for these umis
         df3 <- df2 %>%
             filter(umi %in% selected_umis$strings)
-
-        # worked out which SNP variant we have
-        
-        # in the forward strand, healthy = C
-        df4 <- df3 %>%
-            mutate(nt = str_sub(seq, 54, 54))  # rs12973192 will be 54th base in read
     
-        min_acceptable_fract <- 0.9 # remove ambiguous bases
+        # worked out which base is in the crucial position
+        # in the forward strand, healthy = c
+        df4 <- df3 %>%
+            mutate(nt = str_sub(seq, 38, 38))  # would expect rs12973192 to be 38th nucleotide of aligned sequence 
+    
+        min_acceptable_fract <- 0.95
     
         nt_list <- c()
         fracs <- c()
@@ -168,48 +181,50 @@ for(sample in samples$sample){
                 select(nt, nt_frac) %>%
                 unique() %>%
                 arrange(desc(nt_frac))
-    
+            
             if(max(this_df$nt_frac) >= min_acceptable_fract){
-                this_nt <- this_df$nt[1]  # most abundant because this_dt is ranked by abundance
+                this_nt <- this_df$nt[1]
             } else {
                 this_nt <- "dunno"
             }
-    
+            
             nt_list <- c(nt_list, this_nt)
+    
             fracs <- c(fracs, max(this_df$nt_frac))
+    
         }
     
         final_df <- data.frame(umis = selected_umis$strings, nt = nt_list) %>%
             group_by(nt) %>%
             mutate(n = n()) %>%
-            mutate(risk = ifelse(nt == "G", "Risk", "Non-Risk")) %>%  # in forward genomic strand, G is risk
+            mutate(risk = ifelse(nt == "G", "Risk", "Non-Risk")) %>%  # in bam file, reads are reverse complemented, so correspond to forward genomic
             ungroup() %>%
             select(nt, risk, n) %>%
             unique() %>%
             mutate(sample_name = sample)
     
-        if(sample == samples$sample[1]){
+        if(sample == samples$n[1]){
             all_df <- final_df
         } else {
             all_df <- bind_rows(all_df, final_df)
         }
-    } else { #if no umis
-        # do nothing
+    } else {
+        print(sample)
     }
-
 }
 
-# Make a dataframe to add values to
+
 to_join <- samples %>%
-    select(sample_name = sample)
+    select(sample_name = n)
+
 to_join2 <- bind_rows(to_join %>% mutate(nt = "G", risk = "Risk"),
                       to_join %>% mutate(nt = "C", risk = "Non-Risk"))
-
 
 just_C_or_G_tmp <- all_df %>%
     full_join(to_join2, by = c("nt", "sample_name", "risk")) %>%
     filter(nt == "G"|nt=="C")  %>%
-    filter(!(sample_name == "P17_07"|sample_name =="P35_07"|sample_name=="P64_11"|sample_name == "P47_11")) # remove controls
+    left_join(sample_names, by = "sample_name") %>%
+    filter(!(actual_sample_name %in% c("P17_07","P35_07","P64_11","P47_11"))) # remove controls
 
 just_C_or_G_tmp[is.na(just_C_or_G_tmp)] <- 0
 
@@ -221,4 +236,4 @@ just_C_or_G <- just_C_or_G_tmp %>%
 
 # Write final data out
 
-write_csv(just_C_or_G, "for_plot.csv")
+write_csv(just_C_or_G, "values_for_plot.csv")
